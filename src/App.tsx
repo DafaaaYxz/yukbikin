b4d866673e38f043ac4d98181eeb6f529c97b097
DafaaaYxz <zeuz8686@gmail.com>
2025-11-27 18:21:11 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBot from './pages/CreateBot';
+import Chat from './pages/Chat';
+import './styles/App.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateBot />} />
+          <Route path="/chat/:botId" element={<Chat />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
